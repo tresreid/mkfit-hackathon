1ebd982e7d8a9a0b716fca493a8efe210ff0ec2a
Dan Riley <Daniel.Riley@cornell.edu>
2018-05-25 08:39:24 -0400

    make sure tests pass for testIter > 1 and non-standard widths

--- gplex_mul.h
+++ gplex_mul.h
@@ -1,7 +1,9 @@
 #pragma once
 //#define EIGEN_TEST
 
-void run_naive_mul(int N, int iter);
-void raw_run_naive_mul(int N, int iter);
-void eigen_run_naive_mul(int N, int iter);
+void run_naive_mul(int iter);
+void raw_run_naive_mul(int iter);
+void eigen_run_naive_mul(int iter);
 void propagation_test(int N);
+
+constexpr int Nwidth = 7168;
