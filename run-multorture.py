631be7cf67601b3b29a2859232480a47facc45c9
Tres Reid <tres.reid@gmail.com>
2019-06-05 11:52:59 -0400

    clear output with timing

--- run-multorture.py
+++ run-multorture.py
@@ -2,39 +2,39 @@
 
 from __future__ import print_function
 
-metrics = [
-    "gld_transactions",
-    "flop_count_sp"
-]
 #metrics = [
-#    "flop_count_sp",
-#    "flop_sp_efficiency",
-#    "shared_store_transactions",
-#    "shared_load_transactions",
-#    "local_load_transactions",
-#    "local_store_transactions",
 #    "gld_transactions",
-#    "gst_transactions",
-#    "gld_throughput",
-#    "gst_throughput",
-#    "gld_requested_throughput",
-#    "gld_efficiency",
-#    "gst_requested_throughput",
-#    "gst_efficiency",
-#    "l2_read_transactions",
-#    "l2_write_transactions",
-#    "l2_utilization",
-#    "l1_cache_global_hit_rate",
-#    "l1_shared_utilization",
-#    "l2_l1_read_hit_rate"
-#            ]
+#    "flop_count_sp"
+#]
+metrics = [
+    "flop_count_sp",
+    "flop_sp_efficiency",
+    "shared_store_transactions",
+    "shared_load_transactions",
+    "local_load_transactions",
+    "local_store_transactions",
+    "gld_transactions",
+    "gst_transactions",
+    "gld_throughput",
+    "gst_throughput",
+    "gld_requested_throughput",
+    "gld_efficiency",
+    "gst_requested_throughput",
+    "gst_efficiency",
+    "l2_read_transactions",
+    "l2_write_transactions",
+    "l2_utilization",
+    "l1_cache_global_hit_rate",
+    "l1_shared_utilization",
+    "l2_l1_read_hit_rate"
+            ]
 
 cmd_parts = [
     "nvprof",
     "--csv",
     "--metrics " + ",".join(metrics),
     
-    "./multorture",
+    "./multorture 1",
     ]
 
 import time
@@ -78,18 +78,24 @@ while lines1:
 fout1 = open("profile_test.csv", "w")
 is_first = True
 csv_buffer = ""
+skip=False
 
 while lines1:
     line = lines1.pop(0)
     if "API calls" in line:
         break
     csv_buffer += line + "\n"
-
+    
+   
+    if skip:
+        skip = False
+        continue
     if is_first:
-        print('"time","git_hash",%s' % line, file = fout1)
+        print('"time","git_hash","Metric Name",%s' % line.replace("Name","Kernel").replace("Calls","Invocations"), file = fout1)
         is_first = False
+        skip = True
     else:
-        print('"%s","%s",%s' % (timestamp, git_hash,line), file = fout1)
+        print('"%s","%s","Avg Time",%s' % (timestamp, git_hash,line), file = fout1)
 
 
 while lines:
@@ -106,10 +112,11 @@ csv_buffer = ""
 
 while lines:
     line = lines.pop(0)
-
-    line.replace("Name","Kernel")
+ 
     csv_buffer += line + "\n"
 
+  
+ 
     if is_first:
         print('"time","git_hash",%s' % line, file = fout)
         is_first = False
@@ -158,8 +165,28 @@ for selected_metric in metrics:#[ "gld_throughput", "flop_sp_efficiency"]:
 
 print("wrote",output_fname)
 fout.close()
+fout1.close()
 import pandas as pd
-csv_dataframe = pd.read_csv(output_fname)
-prof_dataframe = pd.read_csv("profile_test.csv")
-csv_table = csv_dataframe[csv_dataframe['Invocations']==1000].pivot(index="Metric Name", columns='Kernel',values='Avg')
-csv_table.to_csv("avg_"+output_fname)
+csv_dataframe1 = pd.read_csv(output_fname)
+prof_dataframe1 = pd.read_csv("profile_test.csv")
+#new_dataframe = pd.merge(csv_dataframe,prof_dataframe,on='Kernel')
+#new_dataframe.to_csv("data_"+output_fname)
+#new_table = new_dataframe[new_dataframe['Invocations']==1000].pivot(index="Metric Name", columns='Kernel',values='Avg')
+
+csv_dataframe = csv_dataframe1[["Kernel","Invocations","Metric Name", "Avg","Max","Min"]]
+prof_dataframe = prof_dataframe1[["Kernel","Invocations","Metric Name", "Avg","Max","Min"]]
+new_dataframe = pd.concat([csv_dataframe,prof_dataframe])
+
+new_dataframe["Kernel"] = new_dataframe["Kernel"].apply(lambda x: x.split("(")[0].split("<")[0])
+
+#csv_dataframe.to_csv("csv_test.csv")
+#prof_dataframe.to_csv("prof_test.csv")
+#new_dataframe.to_csv("new_test.csv")
+
+new_table = new_dataframe[new_dataframe['Invocations']==1000].pivot(index="Metric Name", columns='Kernel',values='Avg')
+#csv_table.to_csv("csv_"+output_fname)
+#prof_table = prof_dataframe[prof_dataframe['Calls']==1000].pivot(index="Metric Name",columns='Kernel',values='Avg')
+#prof_table.to_csv("prof_"+output_fname)
+##new_table = pd.merge(csv_table,prof_table,on='Kernel')
+#new_table = pd.merge(csv_table,prof_table)
+new_table.to_csv("new_"+output_fname)
